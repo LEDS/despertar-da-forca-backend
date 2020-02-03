6623a78d131db38e4dd47569a4736bd4a8a5dc17
lukasg18 <lukas.gomes2010@gmail.com>
2020-02-03 18:26:25 -0200

    feat(entity): iniciando modelagem

---
+++ src/database/database.providers.ts
@@ -0,0 +1,19 @@
+import { createConnection } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'espm_database',
+    useFactory: async () => await createConnection({
+      type: 'postgres',
+      host: 'rajje.db.elephantsql.com',
+      port: 5402,
+      username: 'plsxcjly',
+      password: 'qs-zVfhAUmvT2bvDJgg9iS_MiPJQeDb0',
+      database: 'plsxcjly',
+      entities: [
+          __dirname + '/../**/*.entity{.ts,.js}',
+      ],
+      synchronize: true,
+    }),
+  },
+];
